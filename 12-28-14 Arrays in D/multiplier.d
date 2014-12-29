fdc79a478503ee83b14def42282794a665623c7a
Chris <croerig@gmail.com>
2014-12-28 21:47:16 -0600

    Added D simple array IO looping program

---
+++ 12-28-14 Arrays in D/multiplier.d
@@ -0,0 +1,88 @@
+import std.stdio;
+import std.algorithm;
+import std.string;
+
+void main()
+{
+    double multiplier  = getMultiplier();
+    double[] factors   = getFactors();
+
+    if( shouldSort() )
+    {
+        writeln("Sorting the factor list\n");
+        sort(factors);
+    }
+
+    writeln("Generating results\n");
+
+    int count;
+    while(count < factors.length)
+    {
+        double factor  = factors[count];
+        double product = getProduct(factor, multiplier);
+
+        printMessage(factor, multiplier, product);
+        ++count;
+    }
+
+    writeln("\nAll done!");
+}
+
+double getNumberFromInput()
+{
+    double number;
+    write("Number to multiply: ");
+    readf(" %s", &number);
+
+    return number;
+}
+
+double[] getFactors()
+{
+    double factor;
+    double[] factors;
+
+    writeln("Enter 0 to continue\n");
+
+    while(true)
+    {
+        factor = getNumberFromInput();
+
+        if(factor == 0) break;
+
+        factors ~= factor;
+    }
+    
+    return factors;
+}
+
+double getMultiplier()
+{
+    double multiplier;
+
+    write("Enter the multiplier: ");
+    readf(" %s", &multiplier);
+
+    return multiplier;
+}
+
+double getProduct(double factor, double multiplier)
+{
+    return factor * multiplier;
+}
+
+void printMessage(double factor, double multiplier, double product)
+{
+    writeln(factor, " * ", multiplier, " = ", product);  
+}
+
+bool shouldSort()
+{
+    int input;
+
+    writeln("Sort the factors list?");
+    write("1 = yes, 0 = no: ");
+    readf(" %s", &input);
+
+    return input == 1;
+}
